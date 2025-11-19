886863b247a060e1ee956558ef401f421156de26
Samurai-05 <samueldossant100@gmail.com>
2025-11-19 16:05:22 +0100

    Added socket connection

--- src/main/java/ch/heigvd/dai/network/ClientNetwork.java
+++ src/main/java/ch/heigvd/dai/network/ClientNetwork.java
@@ -2,16 +2,49 @@ package ch.heigvd.dai.network;
 
 import ch.heigvd.dai.applicationInterface.ClientInterface;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class ClientNetwork {
+
+    private final String HOST;
+    private final int PORT;
+
+    public ClientNetwork(String HOST, int PORT) {
+        this.HOST = HOST;
+        this.PORT = PORT;
+    }
+
+    public ClientNetwork() {
+        this("localhost", 7270);
+    }
 
     public int run() {
 
         // connect to socket etc
 
-        ClientInterface clientInterface = new ClientInterface();
+        ClientInterface ui = new ClientInterface();
 
-        clientInterface.showInterface();
-        clientInterface.getUserInput();
+        try (Socket socket = new Socket(HOST, PORT);
+             BufferedReader in =
+                     new BufferedReader(
+                             new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+             BufferedWriter out =
+                     new BufferedWriter(
+                             new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8)); )
+        {
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
 
         return 0;
     }
