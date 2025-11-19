196763544a77cfc3ca94e8295462acfee3565a0a
Samurai-05 <samueldossant100@gmail.com>
2025-11-19 16:10:38 +0100

    Moved socket creation into the constructor

--- src/main/java/ch/heigvd/dai/network/ClientNetwork.java
+++ src/main/java/ch/heigvd/dai/network/ClientNetwork.java
@@ -14,9 +14,23 @@ public class ClientNetwork {
     private final String HOST;
     private final int PORT;
 
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+
     public ClientNetwork(String HOST, int PORT) {
         this.HOST = HOST;
         this.PORT = PORT;
+
+        try{
+            socket = new Socket(HOST, PORT);
+            in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            out = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+        }catch(Exception e){
+
+        }
     }
 
     public ClientNetwork() {
@@ -28,23 +42,6 @@ public class ClientNetwork {
         // connect to socket etc
 
         ClientInterface ui = new ClientInterface();
-
-        try (Socket socket = new Socket(HOST, PORT);
-             BufferedReader in =
-                     new BufferedReader(
-                             new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-             BufferedWriter out =
-                     new BufferedWriter(
-                             new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8)); )
-        {
-
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-
 
         return 0;
     }
