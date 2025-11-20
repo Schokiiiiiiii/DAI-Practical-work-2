8a5b985bba2ab8d4afa84b91a6fb45716677bcbc
Samurai-05 <samueldossant100@gmail.com>
2025-11-20 07:55:31 +0100

    Changed the way the server works
    There must be only one instance of the serverNetwork class. Therefore the client's socket
    
    on the server side is managed in the server controller

--- src/main/java/ch/heigvd/dai/commands/Server.java
+++ src/main/java/ch/heigvd/dai/commands/Server.java
@@ -1,6 +1,9 @@
 package ch.heigvd.dai.commands;
 
 import java.util.concurrent.Callable;
+
+import ch.heigvd.dai.controller.ServerController;
+import ch.heigvd.dai.network.ServerNetwork;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "server", description = "Start the server part of the network game.")
@@ -9,12 +12,16 @@ public class Server implements Callable<Integer> {
   @CommandLine.Option(
       names = {"-p", "--port"},
       description = "Port to use (default: ${DEFAULT-VALUE}).",
-      defaultValue = "6433")
+      defaultValue = "7270")
   protected int port;
 
   @Override
   public Integer call() {
-    throw new UnsupportedOperationException(
-        "Please remove this exception and implement this method.");
+
+      ServerNetwork network = new ServerNetwork(port);
+
+      network.runServer();
+
+      return 1;
   }
 }
